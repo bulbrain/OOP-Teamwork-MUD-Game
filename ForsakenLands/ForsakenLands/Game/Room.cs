58129dfc0ae2602ad970bea9f2064c43275a23db
Zakkgard <zakkgard@gmail.com>
2015-02-17 16:42:18 +0200

    Added a very basic Room class and a position field in Player.cs which will hold the Player position in the world aka Room

---
+++ ForsakenLands/ForsakenLands/Game/Room.cs
@@ -0,0 +1,19 @@
+﻿namespace ForsakenLands
+{
+    public class Room
+    {
+        public Room()
+        {
+            // todo
+        }
+
+        public uint Id { get; private set; }
+        public string RoomDescription { get; private set; }
+
+        public uint SouthExit { get; private set; }
+        public uint NorthExit { get; private set; }
+        public uint WestExit { get; private set; }
+        public uint EastExit { get; private set; }
+
+    }
+}
