a1efef879f965759c4adc1ee19862be8832eef44
Zakkgard <zakkgard@gmail.com>
2015-02-17 17:16:21 +0200

    re-update Room.cs

--- ForsakenLands/ForsakenLands/Game/Room.cs
+++ ForsakenLands/ForsakenLands/Game/Room.cs
@@ -1,19 +1,19 @@
-﻿namespace ForsakenLands
-{
-    public class Room
-    {
-        public Room()
-        {
-            // todo
-        }
+﻿﻿namespace ForsakenLands
+ {
+     public class Room
+     {
+         public Room()
+         {
+             // todo
+         }
 
-        public uint Id { get; private set; }
-        public string RoomDescription { get; private set; }
+         public uint Id { get; private set; }
+         public string RoomDescription { get; private set; }
 
-        public uint SouthExit { get; private set; }
-        public uint NorthExit { get; private set; }
-        public uint WestExit { get; private set; }
-        public uint EastExit { get; private set; }
+         public uint SouthExit { get; private set; }
+         public uint NorthExit { get; private set; }
+         public uint WestExit { get; private set; }
+         public uint EastExit { get; private set; }
 
-    }
-}
+     }
+ }
