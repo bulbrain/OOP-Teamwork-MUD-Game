be34b727afbee201ccadad835435dc10de811496
Zakkgard <zakkgard@gmail.com>
2015-02-17 17:24:08 +0200

    modified StartGame() method to reflect the change in Game.cs

--- ForsakenLands/ForsakenLands/Program.cs
+++ ForsakenLands/ForsakenLands/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace ForsakenLands
+﻿namespace ForsakenLands
 {
     class Program
     {
@@ -11,7 +6,7 @@ namespace ForsakenLands
         {
 
             Game game = new Game();
-            game.startGame();
+            game.StartGame();
         }
     }
 }
