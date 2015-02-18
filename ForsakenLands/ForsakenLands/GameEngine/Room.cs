4f0daf3dbcca7eb55a0275b352d225105c079438
Zakkgard <zakkgard@gmail.com>
2015-02-18 21:54:24 +0200

    Added a constructor for the Room class, along with references to the RoomType enum, a list of items that the Room will hold and a simple WIP method to add an item to that particular room.

--- ForsakenLands/ForsakenLands/GameEngine/Room.cs
+++ ForsakenLands/ForsakenLands/GameEngine/Room.cs
@@ -1,10 +1,21 @@
-﻿﻿namespace ForsakenLands.GameEngine
+﻿namespace ForsakenLands.GameEngine
  {
+     using System;
+     ﻿using System.Collections.Generic;
+
+     using Items;
      public class Room
      {
-         public Room()
+         private const byte ROOM_MAX_ITEMS = 4;
+         public Room(uint id, string name, RoomType roomType, uint southExit, uint northExit, uint westExit, uint eastExit)
          {
-             // todo
+             this.Id = id;
+             this.Name = name;
+             this.RoomType = roomType;
+             this.SouthExit = southExit;
+             this.NorthExit = northExit;
+             this.WestExit = westExit;
+             this.EastExit = eastExit;
          }
 
          public uint Id { get; private set; }
@@ -14,6 +25,22 @@
          public uint NorthExit { get; private set; }
          public uint WestExit { get; private set; }
          public uint EastExit { get; private set; }
+
+         public RoomType RoomType { get; private set; }
+
+         public List<Item> RoomItems { get; private set; }
+
+         public void AddItemToRoom(Item item)
+         {
+             if (this.RoomItems.Count < ROOM_MAX_ITEMS)
+             {
+                 this.RoomItems.Add(item);
+             }
+             else
+             {
+                 throw new NotImplementedException("Needs to be implemented");
+             }
+         }
 
      }
  }
