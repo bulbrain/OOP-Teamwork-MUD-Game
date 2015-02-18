46ac6c64f1ee3be49a2c2843e4d66e2558d3af4b
Georgi Stoyanov <gstoyanov@live.com>
2015-02-18 14:24:31 +0200

    added assemblyInfo.cs

--- ForsakenLands/ForsakenLands/Properties/AssemblyInfo.cs
+++ ForsakenLands/ForsakenLands/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ForsakenLands")]
+[assembly: AssemblyTitle("ForsakenLands.Properties")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ForsakenLands")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("VIVACOM")]
+[assembly: AssemblyProduct("ForsakenLands.Properties")]
+[assembly: AssemblyCopyright("Copyright © VIVACOM 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7d8db726-6ec5-403d-ac01-4ecf51eacb3d")]
+[assembly: Guid("af5bc240-db75-4e91-bbb1-d1f129f47e84")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
