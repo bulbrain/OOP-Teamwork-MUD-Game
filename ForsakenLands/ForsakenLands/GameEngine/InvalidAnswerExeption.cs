aca3277db9db264ddf396d4d6eb5f48a8b6269f0
bulbrain <georgi_d_g1@abv.bg>
2015-02-26 23:24:26 +0100

    added custom exeption

---
+++ ForsakenLands/ForsakenLands/GameEngine/InvalidAnswerExeption.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForsakenLands.GameEngine
+{
+    public class InvalidAnswerExeption : Exception
+    {
+        public InvalidAnswerExeption()
+        {
+        }
+        public InvalidAnswerExeption(string message)
+            : base(message)
+        {
+        }
+
+    }
+}
