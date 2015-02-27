47345e6e1f0a0892a4dfa7c792e3eef569d2a824
ozazza <ozvacheva@gmail.com>
2015-02-27 03:22:20 +0200

    Added CustomException/InvalidUserInputException.cs

---
+++ ForsakenLands/ForsakenLands/CustomException/InvalidUserInputException.cs
@@ -0,0 +1,20 @@
+﻿namespace ForsakenLands.CustomException
+{
+    using System;
+
+    public class InvalidUserInputException : ApplicationException
+    {
+        public InvalidUserInputException(string msg)
+            : base(msg)
+        {
+        }
+
+        public InvalidUserInputException(string msg, string name)
+            : base(msg + " " + name)
+        {
+            this.ErrorName = name;
+        }
+
+        public string ErrorName { get; set; }
+    }
+}
