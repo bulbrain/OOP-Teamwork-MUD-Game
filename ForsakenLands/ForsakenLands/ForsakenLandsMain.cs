0d947eae2f1aa6811e9fa87a8077abe230352b3c
ozazza <ozvacheva@gmail.com>
2015-02-27 14:01:02 +0200

    Refactoring: Added and Naming: Program to ForsakenLandsMain; ForsakenLandsDiagram.cd

---
+++ ForsakenLands/ForsakenLands/ForsakenLandsMain.cs
@@ -0,0 +1,13 @@
+﻿namespace ForsakenLands
+{
+    using ForsakenLands.GameEngine;
+
+    public class ForsakenLandsMain
+    {
+        public static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+            engine.StartGame();
+        }
+    }
+}
